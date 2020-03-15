d5685b0fb1de802a7dbe43eec322ae61f2b7b53b
Jason Keeling <s2jason@yahoo.com>
2020-03-15 22:01:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,9 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(name)
+    arraytemp = []
+    name.each do 
+      badge_maker(name)
+    end
 end
